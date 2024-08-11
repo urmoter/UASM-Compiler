0422d59a1976674cba359f6d223f32580d67f83a
urmoter <sesdespacito@gmail.com>
2024-08-10 22:30:22 -0400

    Time to restart it all, Kill me.

--- headers/FileHandling.h
+++
@@ -1,9 +0,0 @@
-#ifndef FH_h
-#define FH_h
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-FILE* make_VRAM(char* source);
-
-#endif // !FH_h
--- headers/Interpreter.h
+++
@@ -1,8 +0,0 @@
-#ifndef I_h
-#define I_h
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-
-#endif // !I_h
