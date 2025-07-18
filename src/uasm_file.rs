b54cebf0280b9a4f0525844489c8114eef410345
urmoter <burdconrad@gmail.com>
2025-07-18 02:11:21 -0400

    Enhance UasmFilePath with Display implementation and case-insensitive file extension check

--- src/uasm_file.rs
+++ src/uasm_file.rs
@@ -1,11 +1,18 @@
 use std::path::{Path, PathBuf};
 use std::io;
+use std::fmt;
+
+impl fmt::Display for UasmFilePath {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.0.display())
+    }
+}
 
 pub struct UasmFilePath(PathBuf);
 impl UasmFilePath {
     pub fn new<T: AsRef<str>>(path: T) -> Result<Self, io::Error> {
         let path_ref = path.as_ref();
-        if !path_ref.ends_with(".uasm") {
+        if !path_ref.to_lowercase().ends_with(".uasm") {
             return Err(io::Error::new(
                 io::ErrorKind::InvalidInput,
                 "Not a .uasm file",
